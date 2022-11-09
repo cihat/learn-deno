d446788957a487a2865be8010c7689b24d003be2
Cihat Salik <cihatsalik1@gmail.com>
2022-11-09 22:14:29 +0300

    Fix a ts warning message

--- deno-fresh/routes/api/random-uuid.ts
+++ deno-fresh/routes/api/random-uuid.ts
@@ -1,7 +1,7 @@
 import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers = {
-  GET(req) {
+  GET(_) {
     const uuid = crypto.randomUUID()
     return new Response(JSON.stringify({ uuid }), {
       headers: {
